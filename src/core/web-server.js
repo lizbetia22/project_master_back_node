aac83cd6d9d7c624fdaf2b5832487718c4386536
lizbetia22 <elizavetaice123@gmail.com>
2024-06-22 19:32:48 +0200

    bd

--- src/core/web-server.js
+++ src/core/web-server.js
@@ -67,7 +67,7 @@ class WebServer {
         initializeConfigMiddlewares(this.app);
         initializeErrorMiddlwares(this.app);
        // sequelize.sync();
-       sequelize.sync({force:true});
+       // sequelize.sync({force:true});
     }
 
     tablesConnections() {
