80d466633655c6d7ce5499322a9225872f0a49a5
lizbetia22 <elizavetaice123@gmail.com>
2024-06-24 11:39:54 +0200

    cors

--- src/core/web-server.js
+++ src/core/web-server.js
@@ -45,6 +45,7 @@ class WebServer {
     server;
     constructor() {
         this.app = express();
+        this.app.options('*', cors())
         this.port = process.env.PORT;
         this.server = undefined;
         sequelize.sync()
