66d02c47169c00c4c505910df0e739f6e4166959
lizbetia22 <elizavetaice123@gmail.com>
2024-07-04 20:26:16 +0200

    fix

--- src/controllers/users/user_post_controller.js
+++ src/controllers/users/user_post_controller.js
@@ -63,35 +63,39 @@ router.get('/posts/gammes', async (req, res) => {
         console.log('userPosts:', JSON.stringify(userPosts, null, 2));
         console.log('userGammes:', JSON.stringify(userGammes, null, 2));
 
-        const filteredUsers = userPosts
-            .filter(up => up.User.Role && (up.User.Role.name === 'Workshop' || up.User.Role.name === 'Responsible'))
-            .map(up => ({
-                id: up.id_user,
-                userName: up.User.name,
-                roleName: up.User.Role.name,
-                postName: up.Post.name,
-            }));
-
         const userMap = new Map();
 
-        filteredUsers.forEach(user => {
-            if (!userMap.has(user.id)) {
-                userMap.set(user.id, {
-                    id: user.id,
-                    userName: user.userName,
-                    roleName: user.roleName,
-                    postName: [],
-                    gammeName: []
-                });
-            }
-            const userData = userMap.get(user.id);
-            if (!userData.postName.includes(user.postName)) {
-                userData.postName.push(user.postName);
+        // Ajout des utilisateurs et de leurs publications (posts)
+        userPosts.forEach(up => {
+            if (up.User.Role && (up.User.Role.name === 'Workshop' || up.User.Role.name === 'Responsible')) {
+                if (!userMap.has(up.User.id)) {
+                    userMap.set(up.User.id, {
+                        id: up.User.id,
+                        userName: up.User.name,
+                        roleName: up.User.Role.name,
+                        postName: [],
+                        gammeName: []
+                    });
+                }
+                const userData = userMap.get(up.User.id);
+                if (!userData.postName.includes(up.Post.name)) {
+                    userData.postName.push(up.Post.name);
+                }
             }
         });
 
+        // Ajout des gammes aux utilisateurs
         userGammes.forEach(ug => {
             if (ug.User.Role && (ug.User.Role.name === 'Workshop' || ug.User.Role.name === 'Responsible')) {
+                if (!userMap.has(ug.User.id)) {
+                    userMap.set(ug.User.id, {
+                        id: ug.User.id,
+                        userName: ug.User.name,
+                        roleName: ug.User.Role.name,
+                        postName: [],
+                        gammeName: []
+                    });
+                }
                 const userData = userMap.get(ug.User.id);
                 if (userData && !userData.gammeName.includes(ug.name)) {
                     userData.gammeName.push(ug.name);
@@ -108,6 +112,7 @@ router.get('/posts/gammes', async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
+
 
 
 
